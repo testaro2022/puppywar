e23fc5330deda82a01eb4f81ac405fa07f0baad5
skanae <kanae11277@gmail.com>
2022-09-13 13:16:53 +0900

    HTTPtestのページを追加

--- app/src/main/java/com/example/puppywar/HTTPtestActivity.java
+++ app/src/main/java/com/example/puppywar/HTTPtestActivity.java
@@ -3,6 +3,9 @@ package com.example.puppywar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class HTTPtestActivity extends AppCompatActivity {
 
@@ -10,5 +13,35 @@ public class HTTPtestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_httptest);
+
+        Button btGet = findViewById(R.id.btGet);
+        Button btPost = findViewById(R.id.btPost);
+        Button btClear = findViewById(R.id.btClear);
+        ButtonListener listener = new ButtonListener();
+        btGet.setOnClickListener(listener);
+        btPost.setOnClickListener(listener);
+        btClear.setOnClickListener(listener);
+
     }
+
+    private class ButtonListener implements View.OnClickListener{
+        @Override
+        public void onClick(View view) {
+            TextView response = findViewById(R.id.tvResponse);
+            int id = view.getId();
+            switch (id) {
+                case R.id.btGet:
+                    response.setText("Get");
+                    break;
+                case R.id.btPost:
+                    response.setText("Post");
+                    break;
+                case R.id.btClear:
+                    response.setText("");
+                    break;
+            }
+
+        }
+    }
+
 }
